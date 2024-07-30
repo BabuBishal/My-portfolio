703bb0b622369a50dcdd211d16707b52bd4c6708
BabuBishal <bohorabishal20@gmail.com>
2024-07-30 23:43:37 +0545

    my work

---
+++ src/assets/my-work-data.js
@@ -0,0 +1,12 @@
+
+const work_data = [{
+    w_no: 1,
+    w_name: Work1,
+    w_img: Project1-img
+},
+{
+    w_no: 2,
+    w_name: Work2,
+    w_img: Project2-img
+}
+]
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Portfolio.css'
+
+const Portfolio = () => {
+  return (
+    <div className='portfolio'>
+      <div className="work-title">
+        <h1>My Works</h1>
+      </div>
+      <div className="work-container">
+        </div>        
+    </div>
+  )
+}
+
+export default Portfolio
