af227b3f8b556465394cb88110cebe5324724e4b
BabuBishal <bohorabishal20@gmail.com>
2024-07-31 15:05:05 +0545

    M work f

--- src/assets/my-work-data.js
+++ src/assets/my-work-data.js
@@ -1,12 +1,41 @@
+import Project1 from './Project1-img.png'
+import Project2 from  './Project2-img.png'
+import Project3 from './Project3-img.png'
+import Project4 from './Project4-img.png'
+import Project5 from './Project5-img.png'
+import Project6 from './Project6-img.png'
+
 
 const work_data = [{
     w_no: 1,
-    w_name: Work1,
-    w_img: Project1-img
+    w_name: "Work1",
+    w_img: Project1
 },
 {
     w_no: 2,
-    w_name: Work2,
-    w_img: Project2-img
+    w_name: "Work2",
+    w_img: Project2
+},
+{
+    w_no: 3,
+    w_name: "Work3",
+    w_img: Project3
+},
+{
+    w_no: 4,
+    w_name: "Work4",
+    w_img: Project4
+},
+{
+    w_no: 5,
+    w_name: "Work5",
+    w_img: Project5
+},
+{
+    w_no: 6,
+    w_name: "Work6",
+    w_img: Project6
 }
 ]
+
+export default work_data;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import './Portfolio.css'
+import my_work from '../../assets/my-work-data'
+import arrow_icon from '../../assets/arrow-icon.png'
 
 const Portfolio = () => {
   return (
     <div className='portfolio'>
       <div className="work-title">
         <h1>My Works</h1>
+        <hr />
       </div>
       <div className="work-container">
-        </div>        
+        {my_work.map((work, index)=>{
+          return <img key={index} src={work.w_img} alt="" />
+        })}
+      </div> 
+        <div className="work-showmore">
+          <p>Show more</p>
+          <img src={arrow_icon} alt="" />  
+        </div>       
     </div>
   )
 }
