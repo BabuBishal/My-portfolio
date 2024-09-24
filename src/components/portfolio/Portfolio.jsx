ee2442c7c109209caeaa8e78172f38058e57579b
BabuBishal <bohorabishal20@gmail.com>
2024-09-24 17:07:32 +0545

    previous change

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
-import './Portfolio.css'
-import my_work from '../../assets/my-work-data'
-import arrow_icon from '../../assets/arrow-icon.png'
+import React from "react";
+import "./Portfolio.css";
+import my_work from "../../assets/my-work-data";
+import arrow_icon from "../../assets/arrow-icon.png";
 
 function openInNewTab(url) {
-  window.open(url, '_blank').focus();
+  window.open(url, "_blank").focus();
 }
 
 const Portfolio = () => {
   return (
-    <div id='portfolio' className='portfolio'>
+    <div id="portfolio" className="portfolio">
       <div className="work-title">
         <h1>My Works</h1>
         <hr />
       </div>
       <div className="work-container">
-        {my_work.map((work, index)=>{
-          return<a href={work.url} onClick={() => openInNewTab(work.url)}>
-            <img key={index} src={work.w_img} alt="" />
-          </a> 
+        {my_work.map((work, index) => {
+          return (
+            <a
+              key={index}
+              href={work.url}
+              onClick={(e) => {
+                e.preventDefault();
+                openInNewTab(work.url);
+              }}
+            >
+              <img src={work.w_img} alt="" />
+            </a>
+          );
         })}
-      </div> 
-        <div className="work-showmore">
-          <p>Show more</p>
-          <img src={arrow_icon} alt="" />  
-        </div>       
+      </div>
+      <div className="work-showmore">
+        <p>Show more</p>
+        <img src={arrow_icon} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
