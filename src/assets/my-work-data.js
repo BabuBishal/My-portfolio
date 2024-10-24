22fb0be9e90c046a1234585c3cdb22ca981369a1
BabuBishal <bohorabishal20@gmail.com>
2024-10-24 13:22:40 +0545

    new project link added

--- src/assets/my-work-data.js
+++ src/assets/my-work-data.js
@@ -1,47 +1,47 @@
-import Project1 from './projects/utube.png'
-import Project2 from  './projects/tetris.png'
-import Project3 from './projects/hangman.png'
-import Project4 from './projects/chatapp.png'
-import Project5 from './projects/travel.png'
-import Project6 from './projects/xora.png'
+import Project1 from "./projects/utube.png";
+import Project2 from "./projects/tetris.png";
+import Project3 from "./projects/hangman.png";
+import Project4 from "./projects/chatapp.png";
+import Project5 from "./projects/travel.png";
+import Project6 from "./projects/xora.png";
 
-
-const work_data = [{
+const work_data = [
+  {
     w_no: 1,
     w_name: "Work1",
     w_img: Project1,
-    url: 'https://utube-bishal.netlify.app/'
-},
-{
+    url: "https://utube-bishal.netlify.app/",
+  },
+  {
     w_no: 2,
     w_name: "Work2",
     w_img: Project2,
-    url: 'https://bishal-tetris.netlify.app/'
-},
-{
+    url: "https://bishal-tetris.netlify.app/",
+  },
+  {
     w_no: 3,
     w_name: "Work3",
-    w_img: Project3, 
-    url: 'https://bishal-hangman.netlify.app/'
-},
-{
+    w_img: Project3,
+    url: "https://bishal-hangman.netlify.app/",
+  },
+  {
     w_no: 4,
     w_name: "Work4",
     w_img: Project4,
-    url: "https://mychattyy.netlify.app/"
-},
-{
+    url: "https://mychattyy.netlify.app/",
+  },
+  {
     w_no: 5,
     w_name: "Work5",
     w_img: Project5,
-    url: ''
-},
-{
+    url: "",
+  },
+  {
     w_no: 6,
     w_name: "Work6",
-    w_img: Project6, 
-    url: ''
-},
-]
+    w_img: Project6,
+    url: "https://xora-bishal.netlify.app/",
+  },
+];
 
 export default work_data;
